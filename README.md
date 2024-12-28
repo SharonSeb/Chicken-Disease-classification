dc3ce9174dbec7bc99f7ce3fc6ebb581097257c5
Sharon Sebastian <sebastian.sharon@gmail.com>
2024-12-28 17:32:22 +0300

    Data ingestion completed

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
-#CNN Chicken classification
+# CNN Chicken classification
+
+## Workflow
+
+1. Update config.yaml
+2. Update secrets.yaml [optional] (eg. if we have a database which has to be kept secret)
+3. Update params.yaml
+4. Update the entity - create config_entity.py and update
+5. Update the configuration manager in src config (configuration.py)
+6. Update the components
+7. Update the pipeline
+8. Update the main.py
+9. Update the dvc.yaml
+
+
--- main.py
+++ main.py
@@ -1,3 +1,16 @@
-from cnn_classifier import logger
+import os
+import sys
+#from src.cnn_Classifier import logger
+from src.cnn_Classifier.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 
-logger.info("Welcome to my custom log")
+STAGE_NAME = "Data Ingestion Stage"
+
+
+try:
+    #logger.info(f">>>>> stage {STAGE_NAME} started <<<<<")
+    data_ingestion = DataIngestionTrainingPipeline()
+    data_ingestion.main()
+    #logger.info(f">>>>> stage {STAGE_NAME} completed <<<<<\n\nx==========x")
+except Exception as e:
+    #logger.exception(e)
+    raise(e)
