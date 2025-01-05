8a9573fb9c65060606ab80da19018225cb34458b
Sharon Sebastian <sebastian.sharon@gmail.com>
2025-01-05 23:02:16 +0300

    Prepare Base Model completed

--- main.py
+++ main.py
@@ -1,16 +1,35 @@
 import os
 import sys
-#from src.cnn_Classifier import logger
+
+from src.cnn_Classifier import logger
 from src.cnn_Classifier.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
+from src.cnn_Classifier.pipeline.stage_02_prepare_base_model import PrepareBaseModelTrainingPipeline
+import logging
+
+'''if int(sys.version[0]) == 3:
+    assertRaisesRegex
+else:
+    assertRaisesRegexp'''
 
 STAGE_NAME = "Data Ingestion Stage"
 
-
 try:
-    #logger.info(f">>>>> stage {STAGE_NAME} started <<<<<")
+    logging.info(f">>>>> stage {STAGE_NAME} started <<<<<")
     data_ingestion = DataIngestionTrainingPipeline()
     data_ingestion.main()
-    #logger.info(f">>>>> stage {STAGE_NAME} completed <<<<<\n\nx==========x")
+    logging.info(f">>>>> stage {STAGE_NAME} completed <<<<<\n\nx==========x")
 except Exception as e:
-    #logger.exception(e)
+    logging.exception(e)
+    raise(e)
+
+
+STAGE_NAME = "Prepare Base Model"
+
+try:
+    logging.info(f">>>>> stage {STAGE_NAME} started <<<<<")
+    prepare_base_model = PrepareBaseModelTrainingPipeline()
+    prepare_base_model.main()
+    logging.info(f">>>>> stage {STAGE_NAME} completed <<<<<\n\nx==========x")
+except Exception as e:
+    logging.exception(e)
     raise(e)
