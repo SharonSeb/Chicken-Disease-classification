0cf04473fcda5a1d20ca68b3b508acf1120bc162
Sharon Sebastian <sebastian.sharon@gmail.com>
2025-01-07 11:05:26 +0300

    Training done. Model added

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import sys
 from src.cnn_Classifier import logger
 from src.cnn_Classifier.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 from src.cnn_Classifier.pipeline.stage_02_prepare_base_model import PrepareBaseModelTrainingPipeline
+from src.cnn_Classifier.pipeline.stage_03_training import ModelTrainingPipeline
 import logging
 
 '''if int(sys.version[0]) == 3:
@@ -33,3 +34,15 @@ try:
 except Exception as e:
     logging.exception(e)
     raise(e)
+
+
+STAGE_NAME = "Training"
+try: 
+   logger.info(f"*******************")
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+   model_trainer = ModelTrainingPipeline()
+   model_trainer.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
